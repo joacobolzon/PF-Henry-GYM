f01a61e441e918187e26c481c723351cae7c8179
joacobolzon <joaco.bolzon3@gmail.com>
2023-05-09 20:05:14 -0300

    Update productHandlers.js

--- api/src/handlers/productHandlers.js
+++ api/src/handlers/productHandlers.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const {CLOUD_NAME, API_KEY, API_SECRET} = process.env
 
 cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: API_KEY,
-  api_secret: API_SECRET,
+  cloud_name: `${CLOUD_NAME}`,
+  api_key: `${API_KEY}`,
+  api_secret: `${API_SECRET}`,
 });
 
 
