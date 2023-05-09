e9aac1cdf6eabd558a01ff5016ccc40a2a5a658c
joacobolzon <joaco.bolzon3@gmail.com>
2023-05-09 19:58:00 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ require('dotenv').config();
 // } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at `+ process.env.PORT); // eslint-disable-line no-console
   });
